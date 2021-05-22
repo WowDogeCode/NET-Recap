33f62d71de75c6ab57949a98fddf81a2bfbb06c1
WowDogeCode <anil.basol34@gmail.com>
2021-05-23 02:01:01 +0300

    Web API added

---
+++ WebAPI/Controllers/RentalsController.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        IRentalService _rentalService;
+        public RentalsController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _rentalService.GetAll();
+            if (result.Successful)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Rental rental)
+        {
+            var result = _rentalService.Rent(rental);
+            if (result.Successful)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(Rental rental)
+        {
+            var result = _rentalService.Cancel(rental);
+            if (result.Successful)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(Rental rental)
+        {
+            var result = _rentalService.Update(rental);
+            if (result.Successful)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
